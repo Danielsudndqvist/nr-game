1d988ada323a853db3f900a17408b59f432a2094
Daniel Sundqvist <1891sunkan@gmail.com>
2024-06-17 17:53:21 +0000

    number guessing game

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+def number_guessing_game():
+    num = random.randint(1, 100)
+    guesses = 0
+    while guesses<5:
+        answer = int(input("Guess a number between 1 and 100\n"))
+        guesses += 1
+        if answer == num:
+            print("You won!!!")
+            break
+        elif answer < num:
+            print("Try Higher\nYou have used",int(guesses),"out of 5 guesses.")
+        elif answer > num:
+            print("Try Lower\nYou have used",int(guesses),"out of 5 guesses.")
+    else:
+        print("You have no guesses left, the number was",num)
+
