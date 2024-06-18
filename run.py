34d07667498d67a40f9f62bb0385b5287f40b65b
Daniel Sundqvist <1891sunkan@gmail.com>
2024-06-18 16:42:21 +0000

    Uppdating score to spredsheet

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import gspread
 import random
 from google.oauth2.service_account import Credentials
@@ -5,30 +6,36 @@ from google.oauth2.service_account import Credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Scores')
 
-
-def scoreboard():
-    
-    pass
+def fill_scoreboard(username, score, worksheet='Sheet1'):
+    print(f"Updating scoreboard...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row([username, score])
+    print("Scoreboard updated successfully\n")
 
 def number_guessing_game():
     num = random.randint(1, 100)
     guesses = 0
     while guesses < 7:
+        print(num)
         answer = int(input("Guess a number between 1 and 100\n"))
         guesses += 1
         if answer == num:
             print("You won!")
             print(f"Do you want to save your score? You guessed {guesses} out of 7 times correctly.")
-            save_score = str(input("\nEnter your score (yes/no): ").lower())
+            save_score = str(input("(yes/no): \n").lower())
             if save_score == "yes":
-                scoreboard()
+                username = input("Enter your name: ")
+                fill_scoreboard(username, guesses)
             elif save_score == "no":
                 print("Press run to play again.")
-                return  # Exit the function to restart the game
             break
         elif answer < num:
             print("Try Higher\nYou have used", int(guesses), "out of 7 guesses.")
