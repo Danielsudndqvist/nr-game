4fa280ac0db0fe77b41eea7298a197e7aaaca5eb
Daniel Sundqvist <1891sunkan@gmail.com>
2024-06-18 15:30:18 +0000

    added SCOPE

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import gspread
 import random
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
 
 
 def scoreboard():
